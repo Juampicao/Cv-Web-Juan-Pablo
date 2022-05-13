193901818829ffaf22f74365f061e186f7affaf3
juampicao <juanpiparada@gmail.com>
2022-05-12 22:45:50 -0300

    a

--- app.js
+++ app.js
@@ -5,47 +5,23 @@ boton.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
 
-// Efecto Moving Letters
+function addClass() {
+  loader.classList.add("hidden");
+}
 
-// Wrap every letter in a span
-var textWrapper = document.querySelector(".ml11 .letters");
-textWrapper.innerHTML = textWrapper.textContent.replace(
-  /([^\x00-\x80]|\w)/g,
-  "<span class='letter'>$&</span>"
-);
+function spinner() {
+  let divLoader = document.getElementById("divLoader");
+  divLoader.classList.remove("hidden");
+  let loader = document.getElementById("loader");
+  loader.classList.add("desaparecer");
+  setTimeout(addClass, 1000);
+  // loader.fadeOut();
+}
 
-anime
-  .timeline({ loop: true })
-  .add({
-    targets: ".ml11 .line",
-    scaleY: [0, 1],
-    opacity: [0.5, 1],
-    easing: "easeOutExpo",
-    duration: 700,
-  })
-  .add({
-    targets: ".ml11 .line",
-    translateX: [
-      0,
-      document.querySelector(".ml11 .letters").getBoundingClientRect().width +
-        10,
-    ],
-    easing: "easeOutExpo",
-    duration: 700,
-    delay: 100,
-  })
-  .add({
-    targets: ".ml11 .letter",
-    opacity: [0, 1],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: "-=775",
-    delay: (el, i) => 34 * (i + 1),
-  })
-  .add({
-    targets: ".ml11",
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000,
-  });
+setTimeout(spinner, 2000);
+let body = document.getElementById("body");
+
+// Aparece el spinner
+// Esta 2 segundos
+// Desaparecer FadeOUT (add.hidden) o .fadeout
+// Body remove(hidden)
