2070a395afcdca690e18ca6af15ff1f26f6d4f02
juampicao <juanpiparada@gmail.com>
2022-05-20 20:14:43 -0300

    add new proyects

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-const boton = document.querySelector("#boton");
-const menu = document.querySelector("#menu");
+const boton = document.querySelector("#boton"); // ID
+const menu = document.querySelector("#menu"); // ID
 const body = document.getElementById("body");
 
 boton.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
 
-// Loading
+// Codigo para HIDDEN (colocar hidden desde html, toggle desde js) (llamar desde queryselector y #id)
 
-// Switch DarkMode
+const botonShowProyects = document.querySelector("#buttonShowProyects");
+const proyecto1Oculto = document.querySelector("#proyecto1Oculto"); // Es id
+const proyecto2Oculto = document.querySelector("#proyecto2Oculto"); // Es id
+const proyecto3Oculto = document.querySelector("#proyecto3Oculto"); // Es id
+const contenedorProyectoOculto = document.querySelector(
+  "#contenedorProyectoOculto"
+);
 
-// let botonDarkMode = document.getElementsByClassName("switch");
+console.log(
+  proyecto1Oculto,
+  proyecto2Oculto,
+  proyecto3Oculto,
+  contenedorProyectoOculto
+);
 
-// function darkModeOn() {
-//   body.classList.add("darkMode");
-// }
+botonShowProyects.addEventListener("click", () => {
+  proyecto1Oculto.classList.toggle("hidden");
+  proyecto2Oculto.classList.toggle("hidden");
+  proyecto3Oculto.classList.toggle("hidden");
+});
